0e43ebe932ab59193537f688ed176e300a975d14
vikas-011 <vikas.tech011@gmail.com>
2024-08-07 22:09:21 +0530

    add logo size

--- assets/component/topBar.js
+++ assets/component/topBar.js
@@ -1,32 +1,59 @@
-function importTopBar() {
-    const topBarHtml = 
-          `<div class="w-100 float-left top-bar-con main-box">
-              <div class="container">
-                  <div class="top-bar-inner-con d-flex align-items-center justify-content-between">
-                      <div class="left-con">
-                          <a href="mailto:info@luckytourntravels.com"><i class="fa-solid fa-envelope"></i> info@luckytourntravels.com</a>
-                          <!-- left con -->
-                      </div>
-                      <div class="right-con">
-                          <a href="tel:+919109106321"><i class="fa-solid fa-square-phone"></i> +91 9109106321</a>
-                          <!-- right con -->
-                      </div>
-                      <!-- top bar inner con -->
-                  </div>
-                  <!-- container -->
-              </div>
-              <!-- top bar con -->
-          </div>`;
-    const container = document.getElementById("top-bar-container");
+function importNavBar() {
+    const navBarHtml = 
+          ` <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="index.html">
+                        <figure class="mb-0">
+                            <img src="assets/images/lucky-logo.svg" alt="logo-icon">
+                        </figure>
+                    </a>
+
+                    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link p-0" href="index.html">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link p-0" href="about.html">About Us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link p-0" href="destinations.html">Destination</a>
+                                
+                                
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link p-0" href="booking.html">Booking</a>
+                            </li><li class="nav-item">
+                                <a class="nav-link p-0" href="contact.html">Contact</a>
+                            </li>
+                        </ul>
+                        <div class="header-contact">
+                            <ul class="list-unstyled mb-0">
+                                <li><a href="contact.html" class="live-chat-btn d-inline-block"><i class="fa-solid fa-map-location-dot"></i>
+                    Book Now</a></li>
+                                <!-- list unstyled -->
+                            </ul>
+                            <!-- header contact -->
+                        </div>
+                        <!--  -->
+                    </div>
+                </nav>`;
+    const container = document.getElementById("lucky-navbar-container");
     if (container) {
-      container.innerHTML = topBarHtml;
+      container.innerHTML = navBarHtml;
     }
   }
   
   window.addEventListener("load", function () {
-    importTopBar();
+    importNavBar();
   });
   
   // To be used in any HTML page:
-//   export in any html page = <div id="top-bar-container"></div>
-  
+//   export in any html page = <div id="lucky-navbar-container"></div>
+/* <script src="assets/component/topBar.js"></script> */
